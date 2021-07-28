8d290e91cebc5202702efcc860e7e010d41fc84e
MMetelko <mary.a.metelko@vanderbilt.edu>
2021-07-28 13:13:50 -0500

    move docker volume prune call

--- Windows10_setup/StartScripts/02stopJanusgraph.bat
+++ Windows10_setup/StartScripts/02stopJanusgraph.bat
@@ -1,2 +1,3 @@
 CMD /c "docker stop jce-jg jce-es jce-cql"
+CMD /c "docker volume prune -f"
 echo STOPPED JANUSGRAPH
--- Windows10_setup/StartScripts/SwRIRemoveDockers.bat
+++ Windows10_setup/StartScripts/SwRIRemoveDockers.bat
@@ -4,5 +4,3 @@ REM This script will remove the SwRI Tool Dockers (so a fresh start is possible)
 START C:\StartScripts\01removeJenkins.bat
 START C:\StartScripts\02removeJanusgraph.bat
 START C:\StartScripts\06removeMinioDocker.bat
-
-CMD /c "docker volume prune -f"
