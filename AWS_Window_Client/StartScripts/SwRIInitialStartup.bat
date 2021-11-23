828af6cbe49fa085acf842dacd36b5b916f226a2
Metelko <mary.a.metelko@vanderbilt.edu>
2021-11-23 23:24:46 +0000

    No need to wait on jenkins, don't start scripts until linux server is ready

--- AWS_Window_Client/StartScripts/SwRIInitialStartup.bat
+++ AWS_Window_Client/StartScripts/SwRIInitialStartup.bat
@@ -2,11 +2,6 @@ REM This script will build and start the SwRI Tools (building up the docker imag
 REM You must wait for the docker desktop to start before running this script
 @echo off
 
-:WAIT_JENKINS
-netstat -q -n -p TCP | find "<linux server ip>:8080" >nul 2>nul
-if %ERRORLEVEL% neq 0 goto WAIT_JENKINS
-ping <linux server ip> -n 10 > nul
-
 START C:\StartScripts\03loadJanusgraph.bat
 
 TIMEOUT /T 60 /nobreak > nul
--- AWS_Window_Client/StartScripts/SwRIRestart.bat
+++ AWS_Window_Client/StartScripts/SwRIRestart.bat
@@ -1,11 +1,6 @@
 REM This script will restart the SwRI Tools
 @echo off
 
-:WAIT_JENKINS
-netstat -q -n -p TCP | find "<linux server ip>:8080" >nul 2>nul
-if %ERRORLEVEL% neq 0 goto WAIT_JENKINS
-ping <linux server ip> -n 10 > nul
-
 START C:\StartScripts\03loadJanusgraph.bat
 
 TIMEOUT /T 60 /nobreak > nul
