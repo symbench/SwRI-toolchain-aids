01eab7e6d15e94d2c17fbf8536eaabfb64f51fda
MMetelko <mary.a.metelko@vanderbilt.edu>
2021-11-29 17:05:32 -0600

    Updates to AWS startup scripts

--- AWS_Window_Client/StartScripts/03loadJanusgraph.bat
+++ AWS_Window_Client/StartScripts/03loadJanusgraph.bat
@@ -1,9 +1,6 @@
 echo on
 echo LOAD GRAPH ...
-set wfsdir=%CD%
-set wsdir=%wfsdir:\=\\%
-set wbsdir=%wfsdir:\=/%
-pushd "C:\jwork\Agents\janusgraph\bin"
-cmd /k gremlin.bat --verbose -i %WFSDIR%\..\..\StartScripts\load_UAV_graph.groovy
+pushd "C:\JWork\Agents\janusgraph\bin"
+cmd /k gremlin.bat --verbose -i "C:\StartScripts\load_UAV_graph.groovy"
 popd
 echo GRAPH LOAD COMPLETE
--- AWS_Window_Client/StartScripts/11startGraphVisualizer.bat
+++ AWS_Window_Client/StartScripts/11startGraphVisualizer.bat
@@ -1,10 +1,7 @@
 
 echo STARTING GREMLIN-VISUALIZER ...
-cd "C:\Tools\gremlin-visualizer"
+CD /D "C:\gremlin-visualizer"
 cmd /c npm start
-:WAIT_GREMLIN_VIZ
-netstat -q -n -p TCP | find ":3000" >nul 2>nul
-if %ERRORLEVEL% neq 0 goto WAIT_GREMLIN_VIZ
-ping 127.0.0.1 -n 10 > nul
+TIMEOUT /T 60 /nobreak > nul
 START "" http://localhost:3000
-REM TO start looking at graph, use gremlin query: g.V().hasLabel('[avm]Design')
+REM TO start looking at graph, use gremlin query: g.V().has('VertexLabel','[avm]Design')
