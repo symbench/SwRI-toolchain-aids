1139fa1b5f436ba6c23055ed19a81e3d3299c0e2
MMetelko <mary.a.metelko@vanderbilt.edu>
2021-12-17 18:16:25 -0600

    Updates to startup scripts and inclusion of current Cloudformation template.  Template is still a work in progress (TODO list at end of file)

--- AWS_Linux_Server/start-dockers.sh
+++ AWS_Linux_Server/start-dockers.sh
@@ -4,3 +4,8 @@ set -e
 docker-compose -f /opt/NewDeploy/docker/jenkins-pipeline/docker-compose.yaml up -d
 docker-compose -f /opt/NewDeploy/docker/janusgraph-cql-es/docker-compose.yaml up -d
 docker-compose -f /opt/NewDeploy/docker/minio/docker-compose.yaml up -d
+SETUP_IP="<saved linux server IP>"
+CURRENT_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+sleep 10
+docker exec -d jenkins sed -i "s/$SETUP_IP/$CURRENT_IP/g" /var/jenkins_home/jenkins.model.JenkinsLocationConfiguration.xml
+echo "Updated Jenkins URL to $CURRENT_IP"
--- AWS_Window_Client/StartScripts/updateServerIP.bat
+++ AWS_Window_Client/StartScripts/updateServerIP.bat
@@ -5,4 +5,6 @@ echo To: %1
 sed -i "s/%AWS_AMI_SERVER_IP%/%1/g" C:\StartScripts\05openJenkins.bat
 sed -i "s/%AWS_AMI_SERVER_IP%/%1/g" C:\StartScripts\04startBigHoss.bat
 sed -i "s/%AWS_AMI_SERVER_IP%/%1/g" C:\gremlin-visualizer\package.json
+sed -i "s/%AWS_AMI_SERVER_IP%/%1/g" C:\JWork\Agents\janusgraph\conf\remote-objects.yaml
+sed -i "s/%AWS_AMI_SERVER_IP%/%1/g" C:\JWork\Agents\janusgraph\conf\remote.yaml
 del sed*
