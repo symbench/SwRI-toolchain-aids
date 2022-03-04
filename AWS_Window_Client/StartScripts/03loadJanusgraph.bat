708c1f31c197234615df07178acdc8ef6bdb3807
MMetelko <mary.a.metelko@vanderbilt.edu>
2022-03-04 17:26:07 -0600

    Update load graph, need to test on AWS (works on HW system)

--- AWS_Window_Client/StartScripts/03loadJanusgraph.bat
+++ AWS_Window_Client/StartScripts/03loadJanusgraph.bat
@@ -1,6 +1,7 @@
 echo on
 echo LOAD GRAPH ...
+set wfsdir=%CD%
 pushd "C:\JWork\Agents\janusgraph\bin"
-cmd /k gremlin.bat --verbose -i "C:\StartScripts\load_uam_graph.groovy"
+cmd /k gremlin.bat --verbose -i %WFSDIR%\..\..\StartScripts\load_uam_graph.groovy
 popd
 echo GRAPH LOAD COMPLETE
--- Windows10_setup/StartScripts/03loadJanusgraph.bat
+++ Windows10_setup/StartScripts/03loadJanusgraph.bat
@@ -3,7 +3,7 @@ echo LOAD GRAPH ...
 set wfsdir=%CD%
 set wsdir=%wfsdir:\=\\%
 set wbsdir=%wfsdir:\=/%
-CMD /k "C:\NewDeploy\docker\upload_corpus.bat"
+CMD /c "C:\NewDeploy\docker\upload_corpus.bat"
 echo UPLOADED GRAPH TO jce-jg DOCKER
 pushd "C:\jwork\Agents\janusgraph\bin"
 CMD /k gremlin.bat --verbose -i %WFSDIR%\..\..\StartScripts\load_UAV_graph.groovy
