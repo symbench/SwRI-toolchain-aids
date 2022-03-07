4cea0578d7bd78e43d2c6a7106458ef6d2674370
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2022-03-07 14:43:35 -0600

    Add copying of graphml from client to server

--- Windows10_setup/StartScripts/03loadJanusgraph.bat
+++ Windows10_setup/StartScripts/03loadJanusgraph.bat
@@ -3,6 +3,7 @@ echo LOAD GRAPH ...
 set wfsdir=%CD%
 set wsdir=%wfsdir:\=\\%
 set wbsdir=%wfsdir:\=/%
+CMD /c "copy /y /v C:\jwork\Agents\workspace\UAM_Workflows\ExportedGraphML\*.graphml C:\NewDeploy\graph_ml\."
 CMD /c "C:\NewDeploy\docker\upload_corpus.bat"
 echo UPLOADED GRAPH TO jce-jg DOCKER
 pushd "C:\jwork\Agents\janusgraph\bin"
