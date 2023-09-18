24f64b1570dc165eebb8884ae699c3df151314e3
Mary Metelko <mary.a.metelko@vanderbilt.edu>
2023-09-18 14:09:16 -0500

    Move `--verbose` flag to end of command

--- Windows10_setup/StartScripts/03loadJanusgraph-nographml.bat
+++ Windows10_setup/StartScripts/03loadJanusgraph-nographml.bat
@@ -3,6 +3,6 @@ echo LOAD GRAPH ...
 pushd "C:\jwork\Agents"
 CMD /c "python .\workspace\UAM_Workflows\ModelAuthoring\runComponentTest.py localhost .\uam-uav-models\ACM\ .\symcps-mapper\ --adm .\uam-uav-models\ADM\ --graphml .\uam-uav-models\.tmp --clear"
 pushd "C:\jwork\Agents\janusgraph\bin"
-CMD /k gremlin.bat --verbose -i C:\StartScripts\load_gremlin_connect.groovy
+CMD /k gremlin.bat -i C:\StartScripts\load_gremlin_connect.groovy --verbose 
 popd
 echo GRAPH LOAD COMPLETE
--- Windows10_setup/StartScripts/03loadJanusgraph.bat
+++ Windows10_setup/StartScripts/03loadJanusgraph.bat
@@ -4,6 +4,6 @@ CMD /c "copy /y /v C:\jwork\Agents\uam-uav-models\ExportedGraphML\*.graphml C:\N
 CMD /c "C:\NewDeploy\docker\upload_corpus.bat"
 echo UPLOADED GRAPH TO jce-jg DOCKER
 pushd "C:\jwork\Agents\janusgraph\bin"
-CMD /k gremlin.bat --verbose -i C:\StartScripts\load_all_graph.groovy
+CMD /k gremlin.bat -i C:\StartScripts\load_all_graph.groovy --verbose 
 popd
 echo GRAPH LOAD COMPLETE
